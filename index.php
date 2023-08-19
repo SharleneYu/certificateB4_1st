b2b795260dd90382272317345389a5c0c6e5b481
SharleneYu <sharleneyu@hotmail.com>
2023-08-19 18:46:43 +0800

    完成20%：購物流程、最新消息、頁尾版權管理

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php include_once "base.php";
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -15,9 +18,9 @@
     <div id="main">
         <div id="top">
             <a href="index.php">
-                <img src="./icon/0416.jpg">
+                <img src="./icon/0416.jpg" style="width:50%">
             </a>
-            <div style="padding:10px;">
+            <div style="padding:10px; display:inline-block;">
                 <a href="?">回首頁</a> |
                 <a href="?do=news">最新消息</a> |
                 <a href="?do=look">購物流程</a> |
@@ -25,7 +28,9 @@
                 <a href="?do=login">會員登入</a> |
                 <a href="?do=admin">管理登入</a>
             </div>
-            情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~
+
+            <marquee>年終特賣會開跑了   情人節特惠活動</marquee>
+
         </div>
         <div id="left" class="ct">
             <div style="min-height:400px;">
@@ -37,9 +42,30 @@
             </span>
         </div>
         <div id="right">
+
+        <?php
+            $do=$_GET['do']??'main';
+            // [?SYU?]為什麼要將頁面的第一個字母轉大寫交給$table?
+            $table=ucfirst($do);
+            $file="./view/front/{$do}.php";
+
+            //$table的變數如存在，就把這個物件中front功能的頁面show出來
+            if(isset($$table)){
+                $$table->front();            
+            //若帶入的頁面$file是存在的，就include $file；若找不到就載入main.php
+            }else if(file_exists($file)){
+                include $file;
+            }else{
+                include "./view/front/main.php";
+            }
+        ?>
+
+
+
         </div>
-        <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
-            頁尾版權 : </div>
+        <div id="bottom" style="line-height:70px;background:url(./icon/bot.png); color:#FFF;" class="ct">
+            <?=$Bottom->bot();?>
+        </div>
     </div>
 
 </body>
